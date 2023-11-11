cd88e1a964f23a2f357a70a1df4310872bc9481b
Rundaingne <48364843+Rundaingne@users.noreply.github.com>
2023-11-10 22:02:57 -0700

    Initial Commit

---
+++ Fetch-RecipeBrowser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fetch-RecipeBrowser
+//
+//  Created by Erich Kumpunen on 11/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fetch-RecipeBrowser/Fetch_RecipeBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_RecipeBrowserApp.swift
+//  Fetch-RecipeBrowser
+//
+//  Created by Erich Kumpunen on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_RecipeBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
